0310b7db4dacfea90081ae40f1102890e0efab32
KenLD <ken.mauser@live.de>
2017-04-27 11:39:15 +0200

    Add files via upload

---
+++ TutScript.R
@@ -0,0 +1,7 @@
+##### HSDAR Tutorial #####
+
+#Installing required packages
+install.packages("pacman")
+pacman::p_load(raster, mapview, rasterVis, devtools)
+devtools::install_github("krlmlr/here")
+devtools::install_github("cran/hsdar")
