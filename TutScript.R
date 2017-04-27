5351ca92dcc430cd55961d18feba82638193b97f
KenLD <ken.mauser@live.de>
2017-04-27 11:39:42 +0200

    Delete TutScript.R

--- TutScript.R
+++
@@ -1,7 +0,0 @@
-##### HSDAR Tutorial #####
-
-#Installing required packages
-install.packages("pacman")
-pacman::p_load(raster, mapview, rasterVis, devtools)
-devtools::install_github("krlmlr/here")
-devtools::install_github("cran/hsdar")
