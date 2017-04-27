7a17319597610c524599a93c916c11922078a633
KenLD <ken.mauser@live.de>
2017-04-27 12:21:58 +0200

    Update TutScript.R

--- TutScript.R
+++ TutScript.R
@@ -4,11 +4,41 @@
 #Installing required packages
 install.packages("pacman")
 pacman::p_load(raster, mapview, rasterVis, devtools)
+install.packages("backports")
+install.packages("ModelMetrics")
+install.packages("caret")
 devtools::install_github("krlmlr/here")
 devtools::install_github("cran/hsdar")
 
 #Loading required packages
-require(raster, mapview, rasterVis, here, hsdar)
+library(raster)
+library(rasterVis)
+library(mapview)
+library(hsdar)
+require(here)
+
+#-------------- Input ----------------------
 
 #Set WD
 setwd("C:/Users/Ken Mauser/Desktop/Studium Landau/Projekt Umweltwissenschaften/Tutorial Hsdar")
+
+# list files
+file <- here("daten") %>% list.files(pattern = ".tif$", full.names = TRUE)
+file
+
+#read the files in R, nlayers shows the number of bands
+raster <- raster::brick(file[1])
+raster
+
+#-------------- Visualization -----------------
+
+#Quick look at the image, Number from 1 to 5 can be chosen (5 bands sample)
+rasterVis::levelplot(raster[[4]], margin = FALSE, pretty = TRUE)
+mapview(raster[[4]], na.color = "transparent", map.types = "Esri.WorldImagery")
+
+
+#-----------Transforming the Rasterclass so hsdar can use it------------
+
+#apply the wavelength from the metadata?????
+wavelength <- c(404.08,
+
